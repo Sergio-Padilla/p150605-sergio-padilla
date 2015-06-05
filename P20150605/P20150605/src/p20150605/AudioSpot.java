498dc780f0c4f3abf7d69c0204830da0c1d96909
Sergio-Padilla <spadguerrero@gmail.com>
2015-06-05 12:59:18 +0200

    Tras documentar Javadoc Sergio Padilla
    Proyecto tras ser documentado con Javadoc

--- P20150605/P20150605/src/p20150605/AudioSpot.java
+++ P20150605/P20150605/src/p20150605/AudioSpot.java
@@ -2,10 +2,24 @@ package p20150605;
 
 import java.io.File;
 
+/**
+ * Clase destinada a gestionar cuñas publicitarias
+ * 
+ * 
+ * @author Sergio Padilla
+ * @version 1.0.1
+ */
 public class AudioSpot
 {
+	
+	/**
+	 * @param archivo Manejador del archivo que contiene el audio en formato .mp3
+	 * @param duracion Duración del audio en segundos
+	 * @param producto Nombre del producto que se anunciará
+	 * @param anunciante Nombre de la empresa que anuncia el producto
+	 */
     private File archivo;       // manejador para el archivo que contiene el audio (.mp3)
-    private int duracion;       // duraciÃ³n del audio, en segundos
+    private int duracion;       // duración del audio, en segundos
     private String producto;    // nombre del producto anunciado
     private String anunciante;  // nombre de la empresa anunciante
     
@@ -20,34 +34,61 @@ public class AudioSpot
         this.lasterrormsg = "";
     }
     
+    /**
+     * Método que establece los metadatos del anuncio.
+     * 
+     * @param producto Nombre del producto sobre el que se realiza el anuncio
+     * @param anunciante Nombre del anunciante que realiza el anuncio
+     */
     public void setMetaDatos (String producto, String anunciante)
     {
         this.producto = producto;
         this.anunciante = anunciante;
     }
     
+    /**
+     * Método que establece la duración del anuncio.
+     * 
+     * @param duracion La duración del anuncio
+     * @throws IllegalArgumentException Excepción que muestra un error si dura demasiado
+     */
     public void setDuracion(int duracion) throws IllegalArgumentException
     {
         if (duracion<0)
             duracion=0;
         else if (duracion>120)
-            throw new IllegalArgumentException ("DuraciÃ³n demasiado larga");
+            throw new IllegalArgumentException ("Duración demasiado larga");
         this.duracion = duracion;
     }
     
+    /**
+     * Vincula el fichero de audio que se utilizará.
+     * 
+     * @param nombre_archivo Nombre del archivo a utilizar
+     * @return Devuelve true si el archivo existe.
+     */
     public Boolean setArchivo(String nombre_archivo)
     {
         this.archivo = new File(nombre_archivo);
         return this.archivo.exists();
     }
     
+    /**
+     * Pone en la cola de reproducción el anuncio.
+     * 
+     * Si el anuncio no está listo, es decir, le falta algo de lo necesario
+     * no lo pondrá en cola
+     * 
+     * @param cola_reproduccion La cola de reproducción donde se introducirá la cuña
+     * @return Devuelve la cola de reproducción con la cuña introducida
+     */
     public int ProgramaEnCola(Object cola_reproduccion)
     {
         // comprobamos previamente que no falte nada
         int resultado = 0;
         if (this.duracion==0)
         {
-            lasterrormsg = "No se ha establecido duraciÃ³n alguna.";
+            lasterrormsg = "No se ha establecido duración alguna.";
             resultado = -1;
         }
         else if (this.producto.equals(""))
@@ -71,7 +112,7 @@ public class AudioSpot
         {
             try
             {
-                // falta por implementar la programaciÃ³n en la cola_reproducciÃ³n
+                // falta por implementar la programación en la cola_reproducción
                 lasterrormsg = "";
                 return this.duracion;
             }
@@ -85,13 +126,21 @@ public class AudioSpot
             return resultado;
     }
     
+    /**
+     * Exporta el archivo a formato DAW.
+     * 
+     * Solo se exportará si el archivo contiene todos los metadatos
+     * 
+     * @param objeto_daw El objeto formato DAW que se obtendrá
+     * @return Dato a exportar
+     */
     public int ExportaAFormatoDAW(Object objeto_daw)
     {
         // comprobamos previamente que no falte nada
         int resultado = 0;
         if (this.duracion==0)
         {
-            lasterrormsg = "No se ha establecido duraciÃ³n alguna.";
+            lasterrormsg = "No se ha establecido duración alguna.";
             resultado = -1;
         }
         else if (this.producto.equals(""))
@@ -115,7 +164,7 @@ public class AudioSpot
         {
             try
             {
-                // falta por implementar la exportaciÃ³n del audio
+                // falta por implementar la exportación del audio
                 lasterrormsg = "";
                 return 0;
             }
